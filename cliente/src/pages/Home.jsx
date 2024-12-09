79c06795fb8759d3b9784999640a0ffc5249ecd2
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-12-09 17:29:30 -0400

    pdf

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import {jsPDF} from "jspdf";
+import "jspdf-autotable";
 
 export default function Home() {
-
   const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
@@ -29,11 +30,31 @@ export default function Home() {
     }
   }
 
+  // PDF
+  const exportarPDF = () => {
+    const doc = new jsPDF();
+    const tabela = usuarios.map( usuario => [
+      usuario.nome,
+      usuario.email
+    ]);
+    doc.text('Lista de Usuários', 10, 10);
+    doc.autoTable({
+      head: [["Nome", "E-mail"]],
+      body: tabela
+    });
+
+    doc.save("alunos.pdf");
+  }
+
   return (
+    <div>
+      <button onClick={()=> exportarPDF() }>Gerar PDF</button> 
+
     <table>
       <tr>
         <td>Nome</td>
         <td>E-mail</td>
+
       </tr>
       {usuarios.map((usuario) =>
         <tr key={usuario.id}>
@@ -42,6 +63,8 @@ export default function Home() {
           <td><button onClick={() => removerPessoa(usuario.id)}>X</button></td>
         </tr>
       )}
+
     </table>
+    </div>
   );
 }
